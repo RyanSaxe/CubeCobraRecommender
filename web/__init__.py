5e73d2bcf69a68bbe7147c076797c5447c0025d4
Josh Finnie <josh@jfin.us>
2020-04-09 14:45:56 -0400

    adding some basic logging to app.

--- web/__init__.py
+++ web/__init__.py
@@ -1,9 +1,16 @@
+import logging
+
 from flask import Flask, request, jsonify
 
-from web.ml_recommend_web import get_ml_recommend
+from .ml_recommend_web import get_ml_recommend
 
 
 app = Flask(__name__)
+
+if __name__ != "__main__":
+    gunicorn_error_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers.extend(gunicorn_error_logger.handlers)
+    app.logger.setLevel(logging.DEBUG)
 
 
 @app.route('/')
@@ -12,11 +19,23 @@ def api():
     num_recs = request.args.get("num_recs")
     root = request.args.get("root", "https://www.cubecobra.com")
     if not (cube_name and num_recs):
-        return "Need cube_name and num_recs as parameters!"
+        error = "Need cube_name and num_recs as parameters!"
+        app.logger.error(error)
+        return error
     try:
         num_recs = int(num_recs)
     except ValueError:
-        return "num_recs needs to be an integer!"
+        error = "num_recs needs to be an integer!"
+        app.logger.error(error)
+        return error
 
-    results = get_ml_recommend(cube_name, num_recs, root)
+    try:
+        results = get_ml_recommend(cube_name, num_recs, root)
+    except Exception as e:
+        app.logger.error(e)
+        raise e
     return jsonify(results)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, threaded=True)
