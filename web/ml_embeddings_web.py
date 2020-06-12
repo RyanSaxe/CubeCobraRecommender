190cf1d35bf8df2949537eefbe9e7672ddd8e0a7
Devon Richards <friends.devon@gmail.com>
2020-06-12 15:27:51 -0500

    Handle card being None and move computation outside of each function call.

--- web/ml_embeddings_web.py
+++ web/ml_embeddings_web.py
@@ -2,33 +2,36 @@ import json
 import numpy as np
 import unidecode
 from tensorflow import keras
-import urllib.request
+
+int_to_card = json.load(open("./ml_files/recommender_id_map.json", "r"))
+int_to_card = {int(k): v for k, v in int_to_card.items()}
+card_to_int = {v: k for k, v in int_to_card.items()}
+
+model = keras.models.load_model('ml_files/recommender')
+
 
 def get_ml_embeddings(cards, n_decimals=5):
-    int_to_card = json.load(open("./ml_files/recommender_id_map.json", "r"))
-    int_to_card = {int(k): v for k, v in int_to_card.items()}
-    card_to_int = {v: k for k, v in int_to_card.items()}
 
     num_cards = len(int_to_card)
 
-    model = keras.models.load_model('ml_files/recommender')
-
-    card_mtx = np.zeros((len(cards),num_cards))
+    card_mtx = np.zeros((len(cards), num_cards))
 
     doesnt_exist = []
 
-    for i,card in enumerate(cards):
+    for i, card in enumerate(cards):
+        if card is None:
+            doesnt_exist.append(i)
+            continue
         idx = card_to_int.get(unidecode.unidecode(card.lower()))
         if idx is not None:
-            card_mtx[i,idx] = 1
+            card_mtx[i, idx] = 1
         else:
             doesnt_exist.append(i)
-    
+
     embeddings = model.encoder(card_mtx).numpy()
-    
+
     for idx in doesnt_exist:
-        embeddings[idx,:] = np.zeros_like(embeddings[idx,:])
+        embeddings[idx, :] = np.zeros_like(embeddings[idx, :])
 
-    rounded_embs = np.round(embeddings,n_decimals)
+    rounded_embs = np.round(embeddings, n_decimals)
     return rounded_embs.tolist()
-
